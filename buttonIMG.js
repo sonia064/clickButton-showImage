4e40225667e442dcc2d8d5caf1b08122c945e861
Sonia Akter <109096109+sonia064@users.noreply.github.com>
2022-10-04 21:14:02 +0600

    Add files via upload

---
+++ buttonIMG.js
@@ -0,0 +1,29 @@
+         function buuttonPic1()
+         {
+              document.getElementById("pic1").src = "images/img1.jfif";     
+              document.getElementById("pic1").style.width="550px";
+              document.getElementById("pic1").style.height="400px";
+
+         }
+
+         function buuttonPic2()
+         {
+               document.getElementById("pic1").src = "images/img2 (2).jfif";
+               document.getElementById("pic1").style.width="500px";
+               document.getElementById("pic1").style.height="360px";
+         }
+
+         function buuttonPic3()
+         {
+               document.getElementById("pic1").src = "images/img3.jfif";
+               document.getElementById("pic1").style.width="500px";
+               document.getElementById("pic1").style.height="360px";
+         }
+
+         function buuttonPic4()
+         {
+               document.getElementById("pic1").src = "images/img4.jfif";
+               document.getElementById("pic1").style.width="570px";
+               document.getElementById("pic1").style.height="460px";
+         }
+
